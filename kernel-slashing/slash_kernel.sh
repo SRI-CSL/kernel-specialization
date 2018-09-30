b0fc1fd7baf20657ec797c3d93a26026ba7b7850
Muhammad Abubakar <17100056@lums.edu.pk>
2018-09-30 18:38:33 +0500

    added scripts for automatically generating exclusion symbols (undefined and weak)

---
+++ kernel-slashing/slash_kernel.sh
@@ -0,0 +1,7 @@
+cp generateExclusionSymbols.py ${1}/.
+cp slash_kernel.sh ${1}/.
+cd ${1}
+python generateExclusionSymbols.py kernel-manifest.json exclusion_list
+cat ${2} >> exclusion_list 
+OCCAM_LOGFILE=occam.log slash --work-dir=slashing --keep-external=exclusion_list kernel-manifest.json
+./compile_occam_final.sh
